0a95f399cbd35a7331b3b35fc098cebda6f9176c
Melvyn Piau <piau.melvyn@gmail.com>
2023-11-19 20:40:17 -0500

    Flickering todolist
    - Added a little explanation on how to remove the flickering from the console display on each draw. TLDR: We are using too much IO operations between the clear and the entire redraw, we need to centralize everything into a unique buffer that we will clear + draw in one IO call.
    This should remove the flickering, but it will require some work to apply that everywhere.
    Once done, we will be able to focus more on the settings menu.

--- src/main.c
+++ src/main.c
@@ -73,6 +73,24 @@ int main( void )
 
 	print_game_name();
 
+	// Here a little remainder to implement :
+	// We currently have a flickering problem, because we are using printf everywhere.
+	// IO operations are heavy, and clearing then printing X lines can show some flickering.
+	// In order to fix that non-trivial issue, we need to :
+	// - Write in a buffer, and then display it.
+	// - Rewrite the buffer with what you need, and in the same call, clean the X lines to replace.
+	// With only 1 printf each time, the flickering should disappear. We need to impl the buffer logic now
+	// and keep track of the number of lines to clean. So we need to centralize the clean + draw at the same place.
+	// || No flickering here.
+	// V
+	for( int i = 0; i < 50; i++ )
+	{
+		printf( "%sTesting something...\nPlease wait...\nOne last time...\n", i != 0 ? "\033[3A\r\033[J" : "" );
+		Sleep( 500 );
+		printf( "%sHey\nGood?\nYep thx.\n", "\033[3A\r\033[J" );
+		Sleep( 500 );
+	}
+
 	struct GameMenuList menus = {};
 	game_menu_init( &menus );
 
