354bb77e69465d6765282d62b7b2970deac0da02
Melvyn Piau <piau.melvyn@gmail.com>
2023-12-02 13:29:08 -0500

    External terminal and resize handling
    This experimentation was so painful to be honest.
    Having a dedicated terminal wasn't the hardest part ( We now have a console for our game which is nice instead of using the one where the exe has been started ), but handling the resizing of the window was... ugh.
    We can't have a fixed width, so we need to pay attention to the current window size. If too small, display an appropriate message (aka need bigger window ), and otherwise display our game. I have now a combination of escape sequences thata llow us to :
    - Avoid having a scroll bar / display history
    - Glitches if we are resizing rapidly the window
    - Empty lines depending of the cursor position.
    Now that we have a correct handling of the console for the title display, we need to hook the rest of the game in the same pattern.

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+
 #include <windows.h>
 
 
@@ -21,43 +23,127 @@ static void signalHandler( int const signal )
 }
 
 
-void print_game_name( void )
+char const *get_game_name( void )
 {
 	 // I used https://edukits.co/text-art/ for the ASCII generation.
 	 // I needed to add some backslashes afterwards to not break the output with printf.
 
-	term_print( TERM_BOLD_YELLOW,
-		"      __  __           _                      _           _ \n"
-		"     |  \\/  | __ _ ___| |_ ___ _ __ _ __ ___ (_)_ __   __| |\n"
-		"     | |\\/| |/ _` / __| __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` |\n"
-		"     | |  | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |\n"
-		"     |_|  |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|\n"
-		"\n"
-	);
+	return
+		"      __  __           _                      _           _      \n"
+		"     |  \\/  | __ _ ___| |_ ___ _ __ _ __ ___ (_)_ __   __| |     \n"
+		"     | |\\/| |/ _` / __| __/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` |     \n"
+		"     | |  | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |     \n"
+		"     |_|  |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|     \n"
+		"\n";	
 }
 
 
-static void mastermind_singleplayer()
+struct ContentChunk
 {
-	struct Mastermind game;
-	bool resetSettings = true;
+	char *allocContent;
+	usize capacity;
+	usize currPos;
+	usize nbLines;
+};
 
-	for ( ;; )
+
+bool content_chunk_create( struct ContentChunk *const chunk, usize const capacity )
+{
+	assert( capacity > 0 );
+
+	usize const typeSize = sizeof( *chunk->allocContent );
+
+	*chunk = (struct ContentChunk ) {
+		.allocContent = malloc( capacity * typeSize ),
+		.capacity = capacity,
+	};
+
+	return chunk->allocContent != NULL;
+}
+
+
+void content_chunk_append( struct ContentChunk *const chunk, char const *format, ... )
+{
+	usize oldPos = chunk->currPos;
+
+	va_list args;
+	va_start ( args, format );
+	chunk->currPos += vsnprintf( chunk->allocContent + chunk->currPos, chunk->capacity - chunk->currPos, format, args );
+	va_end ( args );
+
+	while ( oldPos < chunk->currPos )
 	{
-		if ( !mastermind_init( &game, resetSettings ) )
-		{
-			printf( "Failed to initialize the game. Stopping.." );
-			exit( 1 );
-		}
-
-		mastermind_game_start( &game );
-		mastermind_destroy( &game );
-
-		// Do you want to play again ?
-		// Y -> loop again + Reset Settings ?
-		// N -> break loop
-		break;
+		if ( chunk->allocContent[oldPos++] == '\n' ) chunk->nbLines++;
 	}
+}
+
+
+void content_chunk_destroy( struct ContentChunk *const chunk )
+{
+	if ( chunk->allocContent )
+	{
+		free( chunk->allocContent );
+	}
+
+	*chunk = (struct ContentChunk) {};
+}
+
+
+BOOL ctrl_handler( DWORD const ctrlType )
+{
+    /*switch ( ctrlType )
+    {
+	case CTRL_C_EVENT:
+    case CTRL_CLOSE_EVENT:
+    case CTRL_BREAK_EVENT:
+    case CTRL_LOGOFF_EVENT:
+    case CTRL_SHUTDOWN_EVENT:
+	default: 	assert( false ); // Shouldn't happen
+    }*/
+
+	// Call cleanup code.
+
+	exit( 0 );
+}
+
+
+// https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences Useful
+// https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797 
+
+void console_init( void )
+{
+	// Detach from the console that has launched the program to have our own console
+	FreeConsole();
+	AllocConsole();
+
+	term_init();
+	SetConsoleTitleA( "Mastermind Game" );
+	SetConsoleCtrlHandler( ctrl_handler, TRUE );
+	ShowScrollBar( GetConsoleWindow(), SB_VERT, 0 );
+}
+
+
+typedef union vec2u32
+{
+	struct { u32 x; u32 y; };
+	u32 data[2];
+} vec2u32;
+
+
+bool vec2u32_equals( vec2u32 const lhs, vec2u32 const rhs )
+{
+	return lhs.x == rhs.x && rhs.y == lhs.y;
+}
+
+
+bool is_screen_too_small( vec2u32 const screenSize )
+{
+	if ( screenSize.x < 65 || screenSize.y < 20 )
+	{
+		return true;
+	}
+
+	return false;
 }
 
 
@@ -65,12 +151,59 @@ int main( void )
 {
 	// Initialize random. Not great but enough.
 	srand( time( NULL ) );
-
+	console_init();
 	signal( SIGINT, signalHandler );
 
-	term_init();
+	struct ContentChunk gameTitleChunk;
+	content_chunk_create( &gameTitleChunk, 512 );
+	content_chunk_append( &gameTitleChunk, "%s%s%s", S_COLOR_STR[TERM_BOLD_YELLOW], get_game_name(), S_COLOR_STR_RESET );
 
-	print_game_name();
+	vec2u32 screenSizeOld = (vec2u32) { .x = 0, .y = 0 };
+	vec2u32 screenSize = screenSizeOld;
+
+	CONSOLE_SCREEN_BUFFER_INFO info = {};
+	HANDLE stdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+	usize prevDisp = 0;
+
+	bool titleDisplayed = false;
+
+	while ( true )
+	{
+		GetConsoleScreenBufferInfo( stdOut, &info );
+		screenSize = (vec2u32) { .x = info.dwSize.X, .y = info.dwSize.Y };
+
+		if ( !vec2u32_equals( screenSize, screenSizeOld ) )
+		{
+			if ( screenSize.y != screenSizeOld.y )
+			{
+				titleDisplayed = false;
+			}
+		}
+
+		if ( is_screen_too_small( screenSize ) )
+		{
+			if ( !vec2u32_equals( screenSize, screenSizeOld ) )
+			{
+				// H -> cursor top, 2J == clean current screen and 3J == clean scroll history
+				printf( "\033[H\x1B[2J\x1B[3JAt least 100x20 is required.\nCurrent: X = %i / Y = %i\n",  screenSize.x, screenSize.y );
+				prevDisp = 2;
+				titleDisplayed = false;
+			}
+		}
+		else
+		{
+			if ( !titleDisplayed )
+			{
+				printf( "\033[H\x1B[2J\x1B[3J%s", gameTitleChunk.allocContent );
+				prevDisp = gameTitleChunk.nbLines;
+				titleDisplayed = true;
+			}
+		}
+
+		screenSizeOld = screenSize;
+		Sleep( 32 );		
+	}
+	return 0;
 
 	struct GameMenuList menus = {};
 	game_menu_init( &menus );
